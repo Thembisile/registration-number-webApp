3627b80ac0f036ab99f6f88446f3b80aa9da7a0c
Thembisile <shaundamon6@icloud.com>
2018-10-22 10:24:16 +0200

    CW removed as town in the database

--- regTable.sql
+++ regTable.sql
@@ -14,7 +14,6 @@ CREATE TABLE reg(
 INSERT INTO towns (town_name, reg) VALUES ('CAPE TOWN', 'CA');
 INSERT INTO towns (town_name, reg) VALUES ('PAARL', 'CJ');
 INSERT INTO towns (town_name, reg) VALUES ('BELVILLE', 'CY'); 
-INSERT INTO towns (town_name, reg) VALUES ('WORCESTER', 'CW');
 INSERT INTO towns (town_name, reg) VALUES ('GEORGE', 'CAW');
 
 
